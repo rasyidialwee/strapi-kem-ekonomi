7a3f0c5b2f40c4bac668153962fce769a2e3b659
Rasyidi-mbp-nbs <rasyidialwee@gmail.com>
2025-10-03 10:25:59 +0800

    Remove page populate middleware from find route in page API

--- src/api/page/routes/page.ts
+++ src/api/page/routes/page.ts
@@ -6,9 +6,6 @@ import { factories } from '@strapi/strapi';
 
 export default factories.createCoreRouter('api::page.page', {
   config: {
-    find: {
-      middlewares: ['api::page.page-populate'],
-    },
     findOne: {
       middlewares: ['api::page.page-populate'],
     },
